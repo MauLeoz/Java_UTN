543d84a9acc9b88b8c2070129638ae7e43c65e22
MauLeoz <mauricio.leoz.89@gmail.com>
2023-04-16 12:36:17 -0300

    Ejercicio integrador

---
+++ Ej. Integrador Interfaces y Clases Abstractas/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Squirtle squirtle = new Squirtle();
+        Charmander charmander = new Charmander();
+        Bulbasaur bulbasaur = new Bulbasaur();
+        Pikachu pikachu = new Pikachu();
+
+        squirtle.atacarAraniaso();
+        squirtle.atacarHidrobomba();
+        charmander.atacarAraniaso();
+        charmander.atacarLanzallamas();
+        bulbasaur.atacarAraniaso();
+        bulbasaur.atacarDrenaje();
+        pikachu.atacarAraniaso();
+        pikachu.atacarImpactrueno();
+
+
+    }
+
+}
