6d242c0baab8293ad41a3c6dc044bcbac7af3cbb
MauLeoz <mauricio.leoz.89@gmail.com>
2023-05-03 17:57:31 -0300

    "vaio"

--- Pedidos Ya/src/Main.java
+++ Pedidos Ya/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello lll!");
+        System.out.println("Prueba Parcial");
 
         System.out.println();
     }
