7576992f3250160f975524ba14cf2739a733e92e
MauLeoz <mauricio.leoz.89@gmail.com>
2023-05-03 13:28:07 -0300

    AA

---
+++ Aviones/src/Main.java
@@ -0,0 +1,13 @@
+import Models.Avion;
+import Models.Comercial;
+import Models.Privado;
+
+public class Main {
+    public static void main(String[] args) {
+        Comercial avionUno = new Comercial("Patito","Air1",500000,"Super",150,12);
+        Comercial avionDos = new Comercial("Patito","Air1",850000,"Super",150,12);
+        Comercial avionTres = new Comercial("Patito","Air1",500000,"Super",150,12);
+        Privado avionCuatro = new Privado();
+
+    }
+}
