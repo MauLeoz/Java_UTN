524757475fdeb04780de78baa207c478426162c7
MauLeoz <mauricio.leoz.89@gmail.com>
2023-06-05 19:11:27 -0300

    05062023

--- Archivo_2/src/main/java/org/example/Main.java
+++ Archivo_2/src/main/java/org/example/Main.java
@@ -1,8 +1,9 @@
 package org.example;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 
-
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
@@ -27,6 +28,24 @@ public class Main {
                 System.out.println("------------------");
             }
         }
+
+        ObjectMapper objectMapper1 = new ObjectMapper();
+        objectMapper1.enable(SerializationFeature.INDENT_OUTPUT);
+
+
+        String rutaArchivo = "C:\\Users\\Usuario\\IdeaProjects\\Java_UTN\\Java_UTN\\Archivo_2\\prueba.json";
+
+        try (FileWriter fileWriter = new FileWriter(rutaArchivo)) {
+            String json = objectMapper1.writeValueAsString(personas);
+            // Escribe la cadena JSON en el archivo
+            fileWriter.write(json);
+            System.out.println("Archivo JSON creado exitosamente.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
 
     }
 }
